c3672bcd54b66d2869bad67cc3e2c5fe2ae6aae6
jsampayo <jsampayo61@gmail.com>
2025-09-28 21:42:50 -0500

    modulo que contiene horarios, capacidad y dias de canchas

--- Appp.java
+++ Appp.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Appp {
@@ -5,12 +7,17 @@ public class Appp {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
         
-    
-        String[][] tablaReservas = {
-            {"Juan", "Alameda", "11 pm - 12am"}
-        };
+
+        //agregar reserva luego de mostrar panel
+        List<String[]> tablaReservas = new ArrayList<>();
         
-        int numeroReservasActivas = 1;
+        String[] reserva = generarReserva("Pedro","cancha","hola"); //borrar
+        
+        tablaReservas.add(reserva);
+        int numeroReservasActivas = tablaReservas.size();
+
+        
+        
         
         //Menu principal
             System.out.println("----------------------------------------\nSistema de reservar para canchas sinteticas\n----------------------------------------\n1. cliente\n2. Administrador de cancha\n3. Dueño\n4. salir");
@@ -20,51 +27,36 @@ public class Appp {
 
             switch (opcionMenuPrincpial) {
 
-                case 1:
+                case 1 -> {
                     System.out.println("----------------------------------------\nMenu cliente\n----------------------------------------\n1. Ver reservas activas\n2. Reservar una cancha\n3. volver");
                     
                     System.out.print("\nSelecciona la opcion de tu preferencia: ");
                     byte opcionMenuCliente = teclado.nextByte();
 
                     if (opcionMenuCliente == 1 && numeroReservasActivas > 0) {
-                        System.out.println("Actualmente tienes (" + numeroReservasActivas + ") reservas activas\n\n");
-                        System.out.printf("%-10s %-10s %-10s\n", "NOMBRE", "CANCHA", "HORA");
+                        System.out.println("\nActualmente tienes (" + numeroReservasActivas + ") reservas activas\n\n");
+                        System.out.printf("%-10s %-10s %-10s %-10s\n", "Id","NOMBRE", "CANCHA", "HORA");
                         
                         for (String[] fila : tablaReservas){
-                            System.out.printf("%-10s %-10s %-10s\n", fila[0], fila[1], fila[2]);
+                            System.out.printf("%-10s %-10s %-10s %-10s\n", ((fila[0].indexOf(fila[0].charAt(0))+1)),fila[0], fila[1], fila[2]);
                             System.out.println("\n");
                         }
                     } else{System.out.println("No tienes reservas activas en este momento");}
+            }
                     
-                    if (opcionMenuCliente == 2) {
-                        System.out.println("Actualmente tienes (" + numeroReservasActivas + ") reservas activas\n\n");
-                        System.out.printf("%-10s %-10s %-10s\n", "NOMBRE", "CANCHA", "HORA");
-                        
-                        for (String[] fila : tablaReservas){
-                            System.out.printf("%-10s %-10s %-10s\n", fila[0], fila[1], fila[2]);
-                            System.out.println("\n");
-                        }
-                    }//else{System.out.println("No tienes reservas activas en este momento");}
+                   
 
 
 
 
-                    break;
 
-                case 2:
-                    System.out.println("----------------------------------------\nMenu administrador de cancha\n----------------------------------------\n1. Consultar reservas - Modificar reservas\n+ 2. Consultar cliente\n3. volver");
+                case 2 -> System.out.println("----------------------------------------\nMenu administrador de cancha\n----------------------------------------\n1. Consultar reservas - Modificar reservas\n+ 2. Consultar cliente\n3. volver");
 
-                case 3:
-                    System.out.println("----------------------------------------\nMenu cliente\n----------------------------------------\n1. Ver reservas activas\n2. Reservar una cancha\n3. volver");
-                    break;
+                case 3 -> System.out.println("----------------------------------------\nMenu cliente\n----------------------------------------\n1. Ver reservas activas\n2. Reservar una cancha\n3. volver");
 
-                case 4:
-                    System.out.println("----------------------------------------\nMenu Dueño de cancha\n----------------------------------------\n1. Consultar sedes\n2. Consultar reservas\n3. Ver estadisticas\n4. volver");
-                    break;
+                case 4 -> System.out.println("----------------------------------------\nMenu Dueño de cancha\n----------------------------------------\n1. Consultar sedes\n2. Consultar reservas\n3. Ver estadisticas\n4. volver");
 
-                default:
-                    System.out.println("----------------------------------------\nOpcion incorrecta\n----------------------------------------");
-                    break;
+                default -> System.out.println("----------------------------------------\nOpcion incorrecta\n----------------------------------------");
 
 
 
@@ -76,17 +68,42 @@ public class Appp {
         
 
     }
+
+
+    static String[] generarReserva(String nombre, String cancha, String hora){
+        String[] datos = {nombre, cancha, hora};
+        return datos;
+    }
 }
         
 // inicio de sesion con cc para cada tipo de usuario
 /*
 List<String[]> tablaReservasList = new ArrayList<>(); CREACION DEL ARRAY
 
-tablaReservasList.add(new String[]{"Juan", "Alameda", "11 pm – 12am"}); AGG ELEMENTOS
+tablaReservasList.add(new String[]datos); AGG ELEMENTOS
 
 .size() PARA TAMAÑO
 
 guardar usuario por cc para mas facil
+
+
+List<String[]> tablaReservasList = new ArrayList<>();
+
+
+        for(String[] dato:tablaReservasList){
+            System.out.println(Arrays.toString(dato));
+        }
+        
+
+        String[] reserva = generarReserva("pedro", "alamadea", "hola");
+
+        tablaReservasList.add(reserva);
+       
+        for(String[] dato:tablaReservasList){
+           System.out.println(Arrays.toString(dato));
+        }
+
+
 
 
 */
