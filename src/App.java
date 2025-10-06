a94b62ca352224dd8d92f01e68ba9b48d361bd2b
Jsampayo <jsampayo61@gmail.com>
2025-10-06 14:45:13 -0500

    estructura de carpetas

--- src/App.java
+++ src/App.java
@@ -3,7 +3,10 @@ public class App {
     public static void main(String[] args) {
 
         //llamar a los metodos aqui
+        Menus.menu_principal();
+        
 
         
     }
-} 
+}
+    
--- src/Inicio_de_sesion.java
+++ src/Inicio_de_sesion.java
@@ -1,5 +1,4 @@
 package src;
-
 public class Inicio_de_sesion {
     public static void main(String[] args) {
         System.out.print("La Cantera Reservas");
