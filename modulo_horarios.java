91d0a867ae64b98a2456a07683d17cea3e14026a
Jsampayo <jsampayo61@gmail.com>
2025-10-08 17:16:09 -0500

    avance

--- modulo_horarios.java
+++ modulo_horarios.java
@@ -3,51 +3,42 @@ import java.util.Scanner;
 public class modulo_horarios {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        String nombreCancha = "LA CANTERA"; 
-        
+        String nombreCancha = "LA CANTERA";
+
         String[] horarioCancha = {
-            "4 PM - 6 PM",
-            "6 PM - 8 PM",
-            "8 PM - 10 PM",
-            "10 PM - 12 AM",
-            
+                "4 PM - 6 PM",
+                "6 PM - 8 PM",
+                "8 PM - 10 PM",
+                "10 PM - 12 AM",
+
         };
 
-        int[] canchas = {10, 14, 16};
+        int[] canchas = { 10, 14, 16 };
 
-        int[] dias = {1,2,3,4,5};
-        
-        
+        int[] dias = { 1, 2, 3, 4, 5 };
+
         System.out.print("Que dia del mes desea reservar (solo escribir el numero) : ");
         int diaReserva = teclado.nextInt();
-        
-        
-        
-        
-        if(diaReserva <30 && diaReserva >0){
-        
-            System.out.println("\nHORARIOS CANCHA (" +nombreCancha+ ") PARA EL DIA " + diaReserva);
+
+        if (diaReserva < 30 && diaReserva > 0) {
+
+            System.out.println("\nHORARIOS CANCHA (" + nombreCancha + ") PARA EL DIA " + diaReserva);
             System.out.println("\n");
-        
-            System.out.printf("%-30s %-20s %-20s\n", "HORA DISPONIBLE","DIA","CAPACIDAD\n");
 
-            for (int j=0; j<horarioCancha.length; j++) {
-                 for (int k = 0; k < canchas.length; k++) {
-                     System.out.printf("%-30s %-20s  %-20s\n", horarioCancha[j], diaReserva, canchas[k]);
-            
-            }System.out.println("\n");
+            System.out.printf("%-30s %-20s %-20s\n", "HORA DISPONIBLE", "DIA", "CAPACIDAD\n");
+
+            for (int j = 0; j < horarioCancha.length; j++) {
+                for (int k = 0; k < canchas.length; k++) {
+                    System.out.printf("%-30s %-20s  %-20s\n", horarioCancha[j], diaReserva, canchas[k]);
+
                 }
+                System.out.println("\n");
+            }
         }
-
-
-
 
         System.out.println("fecha incorrecta");
         System.out.println("hola, esto es una prueba");
 
-
-
-        
-    teclado.close();
-}
+        teclado.close();
+    }
 }
--- src/App.java
+++ src/App.java
@@ -1,12 +1,12 @@
 package src;
+
 public class App {
     public static void main(String[] args) {
+        int idAdmin = 1043634434;
+        String password = "hola";
 
-        //llamar a los metodos aqui
+        // llamar a los metodos aqui
         Menus.menu_principal();
-        
 
-        
     }
 }
-    
