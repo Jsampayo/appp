388879f96549bb1cddbffa8593053aae8cec6ca2
Jsampayo <jsampayo61@gmail.com>
2025-10-09 08:06:47 -0500

    cambios

--- src/Menus.java
+++ src/Menus.java
@@ -1,9 +1,14 @@
 package src;
 
+import java.util.Scanner;
+
 public class Menus {
     // Creé el objeto Scanner dentro de un metodo MAIN para reutilizarlo en los
     // demas metodos, al crearlo en un metodo individual su alcance es solo local
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String Username = Inicio_de_sesion_User(input);
+        System.out.println(Username);
     }
 
     static void menu_principal() {
@@ -11,6 +16,17 @@ public class Menus {
         System.out.println("\n1. Acceder como cliente\n2.Acceder como administrador");
     }
 
+    public static String Inicio_de_sesion_User(Scanner input) {
+        System.out.print("Ingrese su nombre de usuario: ");
+        String Username = input.nextLine();
+        return Username;
+    }
 
+    public static String Inicio_de_sesion_UserPassword(Scanner input) {
+        System.out.println("Ingrese su contraseña:");
+        String UserPassword = input.nextLine();
+        return UserPassword;
+
+    }
 
 }
