956be1351fdb4ce544127511b2b09e8afad4d51f
PabloSampayo <psampayo9@gmail.com>
2025-10-09 08:44:11 -0500

    Agregado Menus de Registro e Inicio de Sesion Usuario

--- src/Menus.java
+++ src/Menus.java
@@ -3,30 +3,44 @@ package src;
 import java.util.Scanner;
 
 public class Menus {
-    // Creé el objeto Scanner dentro de un metodo MAIN para reutilizarlo en los
-    // demas metodos, al crearlo en un metodo individual su alcance es solo local
-    public static void main(String[] args) {
+/*  Creé el objeto Scanner dentro de un metodo MAIN para reutilizarlo en los
+demas metodos, al crearlo en un metodo individual su alcance es solo local*/
+    
+public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         String Username = Inicio_de_sesion_User(input);
         System.out.println(Username);
     }
-
+    //Menu que se muestra al inicializar el programa
     static void menu_principal() {
         System.out.println("La Cantera Reservas");
         System.out.println("\n1. Acceder como cliente\n2.Acceder como administrador");
     }
-
+    //Menu mostrado al elegir la opcion Usuario en el menu principal y acceder
     public static String Inicio_de_sesion_User(Scanner input) {
         System.out.print("Ingrese su nombre de usuario: ");
         String Username = input.nextLine();
         return Username;
     }
-
+    //Menu mostrado al elegir la opcion Usuario en el menu principal y acceder
     public static String Inicio_de_sesion_UserPassword(Scanner input) {
         System.out.println("Ingrese su contraseña:");
         String UserPassword = input.nextLine();
         return UserPassword;
-
     }
+    //Menu mostrado al elegir la opcion Usuario en el menu principal y registrarse
+    public static String Registrar_Username(Scanner input) {
+        System.out.println("Ingrese su contraseña:");
+        String RegistroUser = input.nextLine();
+        return RegistroUser;
+        }
+    //Menu mostrado al elegir la opcion Usuario en el menu principal y registrarse
+    public static String Registro_Password(Scanner input) {
+        System.out.println("Ingrese su contraseña:");
+        String RegistroPassword = input.nextLine();
+        return RegistroPassword;
+        }
+    
+    
 
 }
