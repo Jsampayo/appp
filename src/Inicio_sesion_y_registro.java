bf2f07baa1f0eabe65fc64874d39728f3ce25d43
Pablo Sampayo <psampayo9@gmail.com>
2025-10-11 22:01:17 -0500

    fix(src): arregla error generado al cambiar nombre de archivos sin  actualizarlos en llamada a metodos

---
+++ src/Inicio_sesion_y_registro.java
@@ -0,0 +1,62 @@
+package src;
+
+import java.util.Scanner;
+
+public class Inicio_sesion_y_registro {
+    /*
+     * Creé el objeto Scanner dentro de un metodo MAIN para reutilizarlo en los
+     * demas metodos, al crearlo en un metodo individual su alcance es solo local
+     */
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String Username = Inicio_de_sesion_User(input);
+        System.out.println(Username);
+    }
+
+    // Menu que se muestra al inicializar el programa
+    static byte menu_principal(Scanner input) {
+        System.out.println("\nLa Cantera Reservas");
+        System.out.println("\n1. Acceder como Cliente\n2. Acceder como administrador");
+        System.out.print("\nDigite su opcion: ");
+        byte opcion = input.nextByte();
+        System.out.print("\n");
+        return opcion;
+    }
+
+    // Menu Principal Usuario/Cliente
+    public static byte Menu_cliente(Scanner input) {
+        System.out.println("1. Iniciar Sesión\n2. Registrarse");
+        byte opcion = input.nextByte();
+        return opcion;
+    }
+
+    // Menu mostrado al elegir la opcion Usuario en el menu principal y acceder
+    public static String Inicio_de_sesion_User(Scanner input) {
+        System.out.print("Ingrese su nombre de usuario: ");
+        String Username = input.nextLine();
+        return Username;
+    }
+
+    // Menu mostrado al elegir la opcion Usuario en el menu principal y acceder
+    public static String Inicio_de_sesion_UserPassword(Scanner input) {
+        System.out.print("Ingrese su contraseña: ");
+        String UserPassword = input.nextLine();
+        return UserPassword;
+    }
+
+    // Menu mostrado al elegir la opcion Usuario en el menu principal y registrarse
+    public static String Registrar_Username(Scanner input) {
+        System.out.print("Ingrese su Usuario: ");
+        String RegistroUser = input.nextLine();
+        return RegistroUser;
+    }
+
+    // Menu mostrado al elegir la opcion Usuario en el menu principal y registrarse
+    public static String Registro_Password(Scanner input) {
+        System.out.println("Ingrese su contraseña: ");
+        String RegistroPassword = input.nextLine();
+        return RegistroPassword;
+    }
+
+}
---
+++ src/Mensajes.java
@@ -0,0 +1,45 @@
+package src;
+
+public class Mensajes {
+    
+    
+    //menus para colocar a la hora de que la sesion o de un error o sea correcta para continuar
+    public void Sesion_usuario() {
+        
+        System.out.print("╔═════════════════════════════════╗");
+        System.out.print("║                                 ║");
+        System.out.print("║      Bienvenido cliente.        ║");
+        System.out.print("║                                 ║");
+        System.out.print("╚═════════════════════════════════╝"); 
+    }
+public void Sesion_admin() {
+        System.out.print("╔═════════════════════════════════╗");
+        System.out.print("║                                 ║");
+        System.out.print("║    Bienvenido Administrador.    ║");
+        System.out.print("║                                 ║");
+        System.out.print("╚═════════════════════════════════╝");
+    }
+public void Sesion_error() {
+        System.out.print("╔═════════════════════════════════╗");
+        System.out.print("║                                 ║");
+        System.out.print("║      su usario o contraseña     ║");
+        System.out.print("║         son incorrectos         ║");
+        System.out.print("║                                 ║");
+        System.out.print("╚═════════════════════════════════╝");
+
+    }
+public void Sesion_en_uso() {
+        System.out.print("╔═════════════════════════════════╗");
+        System.out.print("║                                 ║");
+        System.out.print("║       Este usuario ya esta      ║");
+        System.out.print("║             registrado          ║");
+        System.out.print("║                                 ║");
+        System.out.print("╚═════════════════════════════════╝");
+    }
+        //Crear Menu opcion no Valida
+        //Conectar Metodos inicio de sesion User en base al diagrama de flujo utilizando If y imprimiendo lo ingresado por teclado 
+        
+        
+
+
+}
