e6a184b34aa599f2ed92d475c4dc472cb8291aaa
Pablo Sampayo <psampayo9@gmail.com>
2025-10-13 21:31:32 -0500

    refactor(src) refactoriza metodo para inicion de sesion de usuario y crea logica para agregar registros de usuarios al hashmap

--- src/Main.java
+++ src/Main.java
@@ -9,32 +9,17 @@ public class Main {
         String IdAdmin = "1043634434";
         String PasswordAdmin = "hola";
 
-        // Llamada al menu principal y almacenamos una variable con la opcion elegida para elegir el men correspondiente
+        // Llamada al menu principal y almacenamos una variable con la opcion elegida para elegir el menú correspondiente
         Byte opcion = Inicio_sesion_y_registro.menu_principal(input);
 
-        // Si la opcion es 1 llamamos al menu del Usuario y pedimos por teclado si desea Iniciar Sesion o Registrarse
-        // Consumimos Saltos de linea con input.nextLine(); para que no ignore nuestro Scanner 
+        // Si la opcion es 1 llamamos al menu del Usuario 
         if (opcion == 1){
-            byte inicio = Inicio_sesion_y_registro.Menu_cliente(input);
-                if (inicio == 1){
-                    input.nextLine();
-                    String Username = Inicio_sesion_y_registro.Inicio_de_sesion_User(input);
-                    System.out.println("Username "+ Username);
-                    String UserPassword = Inicio_sesion_y_registro.Inicio_de_sesion_UserPassword(input);
-                    System.out.println("Password "+ UserPassword);
-                                }
-                
-                else if (inicio == 2) { 
-                    input.nextLine();
-                    String RegistroUser = Inicio_sesion_y_registro.Registrar_Username(input);
-                    System.out.println("Register Username: "+ RegistroUser);
-                     String RegistroPassword = Inicio_sesion_y_registro.Registro_Password(input);
-                    System.out.println("Register Password: "+ RegistroPassword); 
-                                      }
-                        }
-        
-        // Si la opcion es 2 llamamos pedimos las credenciales directamente ya que el admin no necesita registrarse                         
-       else if (opcion == 2){
+           Inicio_sesion_y_registro.Menu_cliente(input);
+        }
+    
+        //Jesus tu parte (Convertir en metodo)
+        // Si la opcion es 2 llamamos pedimos las credenciales directamente ya que el admin no necesita registrarse       
+        else if (opcion == 2){
             input.nextLine();
             String AdminName = Inicio_sesion_y_registro.AdminUser(input);
             System.out.println("AdminName: "+AdminName);
@@ -44,12 +29,16 @@ public class Main {
         // Validamos directamente que el nombre y contraseña sea el preestablecido con .equals que es la manera correcta de comparar strings  
                 if (! AdminName.equals(IdAdmin) || ! AdminPassword.equals(PasswordAdmin)){
                     Mensajes.Sesion_error();
-                    }
+                }
                 else {
                     Mensajes.Sesion_admin();
-                     }
-            }   
-            }
+                }
+        }   
+                
+    
+    
+    
+    }
 
        
         
--- src/Mensajes.java
+++ src/Mensajes.java
@@ -6,46 +6,54 @@ public class Mensajes {
     //menus para colocar a la hora de que la sesion o de un error o sea correcta para continuar
     public static void Sesion_usuario() {
         
-        System.out.print("╔═════════════════════════════════╗");
-        System.out.print("║                                 ║");
-        System.out.print("║      Bienvenido cliente.        ║");
-        System.out.print("║                                 ║");
-        System.out.print("╚═════════════════════════════════╝"); 
+        System.out.println("╔═════════════════════════════════╗");
+        System.out.println("║                                 ║");
+        System.out.println("║      Bienvenido cliente.        ║");
+        System.out.println("║                                 ║");
+        System.out.println("╚═════════════════════════════════╝"); 
     }
 public static void Sesion_admin() {
-        System.out.print("╔═════════════════════════════════╗");
-        System.out.print("║                                 ║");
-        System.out.print("║    Bienvenido Administrador.    ║");
-        System.out.print("║                                 ║");
-        System.out.print("╚═════════════════════════════════╝");
+        System.out.println("╔═════════════════════════════════╗");
+        System.out.println("║                                 ║");
+        System.out.println("║    Bienvenido Administrador.    ║");
+        System.out.println("║                                 ║");
+        System.out.println("╚═════════════════════════════════╝");
     }
 public static void Sesion_Exitosa() {
-        System.out.print("╔═════════════════════════════════╗");
-        System.out.print("║                                 ║");
-        System.out.print("║       Su registro a sido        ║");
-        System.out.print("║             Exitosó.            ║");
-        System.out.print("║                                 ║");
-        System.out.print("╚═════════════════════════════════╝");
+        System.out.println("╔═════════════════════════════════╗");
+        System.out.println("║                                 ║");
+        System.out.println("║       Su registro a sido        ║");
+        System.out.println("║             Exitosó.            ║");
+        System.out.println("║                                 ║");
+        System.out.println("╚═════════════════════════════════╝");
     }
 public static void Sesion_error() {
-        System.out.print("╔═════════════════════════════════╗");
-        System.out.print("║                                 ║");
-        System.out.print("║      su usario o contraseña     ║");
-        System.out.print("║         son incorrectos.        ║");
-        System.out.print("║                                 ║");
-        System.out.print("╚═════════════════════════════════╝");
+        System.out.println("╔═════════════════════════════════╗");
+        System.out.println("║                                 ║");
+        System.out.println("║      su usario o contraseña     ║");
+        System.out.println("║         son incorrectos.        ║");
+        System.out.println("║                                 ║");
+        System.out.println("╚═════════════════════════════════╝");
 
     }
 public static void Sesion_en_uso() {
-        System.out.print("╔═════════════════════════════════╗");
-        System.out.print("║                                 ║");
-        System.out.print("║      Este usuario ya esta       ║");
-        System.out.print("║           registrado.           ║");
-        System.out.print("║                                 ║");
-        System.out.print("╚═════════════════════════════════╝");
+        System.out.println("╔═════════════════════════════════╗");
+        System.out.println("║                                 ║");
+        System.out.println("║      Este usuario ya esta       ║");
+        System.out.println("║           registrado.           ║");
+        System.out.println("║                                 ║");
+        System.out.println("╚═════════════════════════════════╝");
     }
-        //Crear Menu opcion no Valida
-        //Conectar Metodos inicio de sesion User en base al diagrama de flujo utilizando If y imprimiendo lo ingresado por teclado 
+
+public static void Opción_Invalida() {
+        System.out.println("╔═════════════════════════════════╗");
+        System.out.println("║                                 ║");
+        System.out.println("║        Opcion no valida.        ║");
+        System.out.println("║                                 ║");
+        System.out.println("╚═════════════════════════════════╝");
+    }
+
+        
         
         
 
